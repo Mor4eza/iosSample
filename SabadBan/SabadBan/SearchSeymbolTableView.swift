74843a08f5d6cb657d7123eea354cf1808ca7460
morteza.garedaghi <morteza.garedaghi@b0be970a-c655-48fc-b610-b9ecd9b1ed0f>
2016-08-31 11:41:17 +0000

    SABADBAN-218 2 Add: search Symbol Added

--- SabadBan/SabadBan/SearchSeymbolTableView.swift
+++ SabadBan/SabadBan/SearchSeymbolTableView.swift
@@ -9,46 +9,111 @@
 import UIKit
 import Alamofire
 import Alamofire_Gloss
-class SearchSeymbolTableView: BaseTableViewController {
-
+class SearchSeymbolTableView: BaseTableViewController ,UISearchResultsUpdating , UISearchBarDelegate{
     
-    var symbolName = [String]()
-    var symbolCode = [String]()
-    var symbolFullName = [String]()
+    let searchController = UISearchController(searchResultsController: nil)
+    var shouldShowSearchResults = false
+    var symbolsData = [symbolData]()
+    var filteredSymbol = [symbolData]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.tableView.tableFooterView = UIView()
         getSymbolList()
         
+        
+        searchController.searchResultsUpdater = self
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.sizeToFit()
+        searchController.searchBar.placeholder = "جستجو"
+        searchController.searchBar.delegate = self
+        self.tableView.tableHeaderView = searchController.searchBar
+        
     }
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return symbolName.count
+        if shouldShowSearchResults {
+            return filteredSymbol.count
+        }
+        else {
+            return symbolsData.count
+        }
     }
-
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("symbolsCell", forIndexPath: indexPath) as UITableViewCell
-
-        cell.textLabel?.text = symbolName[indexPath.row]
+        let cell = tableView.dequeueReusableCellWithIdentifier("SymbolsCell", forIndexPath: indexPath)
         
-        cell.detailTextLabel?.text = symbolFullName[indexPath.row]
+        if shouldShowSearchResults {
+            cell.textLabel?.text = filteredSymbol[indexPath.row].name
+            cell.detailTextLabel?.text = filteredSymbol[indexPath.row].fullName
+        }else{
+            cell.textLabel?.text = symbolsData[indexPath.row].name
+            cell.detailTextLabel?.text = symbolsData[indexPath.row].fullName
+        }
         
         return cell
     }
- 
+    
+    
+    //MARK : - Search Controller
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchText == "" {
+            shouldShowSearchResults = false
+            self.tableView.reloadData()
+            
+        }else{
+            shouldShowSearchResults = true
+            self.tableView.reloadData()
+        }
+        
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        shouldShowSearchResults = false
+        self.tableView.reloadData()
+    }
+    
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        if !shouldShowSearchResults {
+            shouldShowSearchResults = true
+            self.tableView.reloadData()
+        }
+        
+        searchController.searchBar.resignFirstResponder()
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        let searchString = searchController.searchBar.text
+        
+        // Filter the data array and get only those countries that match the search text.
+        
+        filteredSymbol = symbolsData.filter{ symbol in
+            
+            return (symbol.name.lowercaseString.containsString(searchString!.lowercaseString) ||
+                symbol.fullName.lowercaseString.containsString(searchString!.lowercaseString))
+        }
+        // Reload the tableview.
+        self.tableView.reloadData()
+    }
+    
+    
+    
     //MARK: - Get Symbol List Service
     
     func getSymbolList() {
-    
+        
         let url = AppTadbirUrl + URLS["getSymbolListAndDetails"]!
         let headers = [
             "Content-Type":"application/json",
@@ -70,10 +135,8 @@ class SearchSeymbolTableView: BaseTableViewController {
                 switch response.result {
                 case .Success(let symbols):
                     for i in 0  ..< symbols.response.symbolDetailsList.count{
-                        self.symbolName.append(symbols.response.symbolDetailsList[i].symbolNameFa)
-                        self.symbolCode.append(symbols.response.symbolDetailsList[i].symbolCode)
-                        self.symbolFullName.append(symbols.response.symbolDetailsList[i].symbolCompleteNameFa)
-                    
+                        self.symbolsData.append(symbolData(name: symbols.response.symbolDetailsList[i].symbolNameFa, fullName: symbols.response.symbolDetailsList[i].symbolCompleteNameFa, code: symbols.response.symbolDetailsList[i].symbolCode))
+                        
                         self.tableView.reloadData()
                     }
                     break
@@ -84,4 +147,14 @@ class SearchSeymbolTableView: BaseTableViewController {
         }
     }
 }
+
+
+struct symbolData {
+    
+    var name = String()
+    var fullName = String()
+    var code = String()
+}
+
+
 
