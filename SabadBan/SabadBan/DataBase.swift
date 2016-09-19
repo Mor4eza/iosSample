5130f1f435585bbad52070ce672f7ce40754dbeb
morteza.garedaghi <morteza.garedaghi@b0be970a-c655-48fc-b610-b9ecd9b1ed0f>
2016-09-19 13:13:10 +0000

    SABADBAN-242 8.0 Add: buy Info Classes edit: changed Some Parameters for bamboo build

--- SabadBan/SabadBan/DataBase.swift
+++ SabadBan/SabadBan/DataBase.swift
@@ -11,17 +11,25 @@ import SQLite
 class DataBase{
     
     var db:Connection!
-    //portfolio Table
+  
     var tblPortfolio:Table!
     var tblSymbolPortfolio:Table!
-    
+    var tblPsBuy:Table!
+      //portfolio Table
     let portfolioName = Expression<String?>("portfolioName")
     let portfolioCode = Expression<Int>("portfolioCode")
     let portfolioOwnerId = Expression<Int>("portfolioOwnerUserId")
-    
+      //symbol Protfolio
     let psCode = Expression<Int>("psCode")
     let symbolPCode = Expression<Int>("portfolioCode")
     let symbolCode = Expression<String>("symbolCode")
+      //psBuy Table
+    
+    let PS_BUY_ID = Expression<Int>("buyId")
+    let PS_BUY_PS_CODE = Expression<Int>("buyPSCode")
+    let PS_BUY_PRICE = Expression<Double>("buyPrice")
+    let PS_BUY_QUANTITY = Expression<Double>("buyQuantity")
+    let PS_BUY_DATE = Expression<String>("buyDate")
     
     
     init(){
@@ -57,7 +65,18 @@ class DataBase{
             t.column(symbolCode)
             })
 
+         //psBuy Table
         
+        
+        tblPsBuy = Table("tbPsBuy")
+        try! db.run(tblPsBuy.create(ifNotExists : true) { t in
+            t.column(PS_BUY_ID, primaryKey: true)
+            t.column(PS_BUY_PS_CODE)
+            t.column(PS_BUY_PRICE)
+            t.column(PS_BUY_QUANTITY)
+            t.column(PS_BUY_DATE)
+            })
+
     }
     
     
@@ -116,6 +135,22 @@ class DataBase{
         
     }
     
+    func getPsCodeBySymbolCode(sCode:String , pCode:Int)-> Int{
+        
+        
+        var pscode = Int()
+        let  currentQuery = tblSymbolPortfolio.select(psCode)
+            .filter(symbolPCode == pCode && symbolCode == sCode)
+        
+        for pn in try!  db.prepare(currentQuery){
+            pscode = pn[psCode]
+        }
+        
+        return pscode
+
+        
+    }
+    
     func updatePortfolioName(newName:String , pCode:Int) {
         
         let portfolio = tblPortfolio.filter(portfolioCode == pCode)
@@ -126,5 +161,38 @@ class DataBase{
         
         let portfoi = tblPortfolio.filter(portfolioCode == id)
         try! db.run(portfoi.delete())
+    }
+    
+    
+     //MARK:- psBuy
+    func addPsBuy(psCode:Int , price:Double , count:Double , date:String){
+        
+        let insert = tblPsBuy.insert(PS_BUY_PS_CODE <- psCode , PS_BUY_PRICE <- price ,  PS_BUY_QUANTITY <- count ,  PS_BUY_DATE <- date)
+        try! db.run(insert)
+    }
+    
+    func getPsBuy(psCode:Int) -> [psBuyModel]{
+        
+        var psBuys = [psBuyModel]()
+        psBuys.removeAll()
+        for buys in try! db.prepare(tblPsBuy.filter(PS_BUY_PS_CODE == psCode)) {
+            
+            psBuys.append(psBuyModel(psId: buys[PS_BUY_ID], psCode: buys[PS_BUY_PS_CODE], psDate: buys[PS_BUY_DATE], psCount: buys[PS_BUY_QUANTITY], psPrice: buys[PS_BUY_PRICE]))
+        }
+        return psBuys
+    }
+    
+    func updatePsBuy(buyCode:Int ,newPrice:Double , newCount:Double , newDate:String){
+        
+        let ps = tblPsBuy.filter(PS_BUY_ID == buyCode)
+        try! db.run(ps.update(PS_BUY_PRICE <- newPrice ,PS_BUY_QUANTITY <- newCount ,PS_BUY_DATE <- newDate ))
+    }
+    
+    
+    func deletePsBuy(buyCode:Int){
+        
+        let ps = tblPsBuy.filter(PS_BUY_ID == buyCode )
+        try! db.run(ps.delete())
+     
     }
 }
